0643ceeee7aae883a2af17bffc8a74d30b558680
ellisman31 <akos99@protonmail.com>
2024-04-22 20:33:53 +0200

    Game model creation

---
+++ src/main/java/com/src/movieandgamereview/model/Game.java
@@ -0,0 +1,20 @@
+package com.src.movieandgamereview.model;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.jdbc.core.mapping.AggregateReference;
+import org.springframework.data.relational.core.mapping.Table;
+
+@Data
+@Table("game")
+public class Game {
+    @Id
+    private Long id;
+    private AggregateReference<Information, Long> information;
+    private AggregateReference<GameGenre, Long> gameGenre;
+
+    public Game(AggregateReference<Information, Long> information, AggregateReference<GameGenre, Long> gameGenre) {
+        this.information = information;
+        this.gameGenre = gameGenre;
+    }
+}
