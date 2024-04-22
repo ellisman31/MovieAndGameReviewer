6c951b8d90a3b5719b521054ceedef0684f1a608
ellisman31 <akos99@protonmail.com>
2024-04-22 20:34:11 +0200

    Information model creation

---
+++ src/main/java/com/src/movieandgamereview/model/Information.java
@@ -0,0 +1,38 @@
+package com.src.movieandgamereview.model;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.jdbc.core.mapping.AggregateReference;
+import org.springframework.data.relational.core.mapping.Table;
+
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+
+@Data
+@Table("information")
+public class Information {
+    @Id
+    private Long id;
+    private String title;
+    private String description;
+    private LocalDate releaseDate;
+    private int budget;
+    private AggregateReference<Rate, Long> rate;
+    private AggregateReference<Language, Long> language;
+    private AggregateReference<Review, Long> review;
+    private AggregateReference<Director, Long> director;
+    private Set<Actor> actors;
+
+    public Information(String title, String description, LocalDate releaseDate, int budget, AggregateReference<Rate, Long> rate, AggregateReference<Language, Long> language, AggregateReference<Review, Long> review, AggregateReference<Director, Long> director) {
+        this.title = title;
+        this.description = description;
+        this.releaseDate = releaseDate;
+        this.budget = budget;
+        this.rate = rate;
+        this.language = language;
+        this.review = review;
+        this.director = director;
+        this.actors = new HashSet<>();
+    }
+}
