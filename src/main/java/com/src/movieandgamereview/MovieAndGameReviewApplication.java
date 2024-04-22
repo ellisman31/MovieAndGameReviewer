73434d031c599df085d1462ec091ebb64e699cef
ellisman31 <akos99@protonmail.com>
2024-04-22 20:39:17 +0200

    Initialize Spring Boot

---
+++ src/main/java/com/src/movieandgamereview/MovieAndGameReviewApplication.java
@@ -0,0 +1,13 @@
+package com.src.movieandgamereview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieAndGameReviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieAndGameReviewApplication.class, args);
+	}
+
+}
