cec5b47d910fdcbfe3d3e4b5585293e641b98300
ellisman31 <akos99@protonmail.com>
2024-05-09 19:49:07 +0200

    UserGroup Service adjustments

--- src/main/java/com/src/movieandgamereview/service/UserGroupService.java
+++ src/main/java/com/src/movieandgamereview/service/UserGroupService.java
@@ -1,22 +1,26 @@
 package com.src.movieandgamereview.service;
 
+import com.src.movieandgamereview.dto.UserDTO;
 import com.src.movieandgamereview.dto.UserGroupDTO;
+import com.src.movieandgamereview.model.User;
 import com.src.movieandgamereview.model.UserGroup;
 import com.src.movieandgamereview.repository.UserGroupRepository;
-import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 @Service
-@AllArgsConstructor
 public class UserGroupService {
-
+    @Autowired
     private UserGroupRepository userGroupRepository;
+    @Autowired
+    private UserService userService;
 
-    public List<UserGroupDTO> getAllUserGroupDTO() {
+    public List<UserGroupDTO> getAllUserGroup() {
         return ((List<UserGroup>) userGroupRepository.findAll()).stream().map(this::convertToUserGroupDTO).collect(Collectors.toList());
     }
 
@@ -25,15 +29,35 @@ public class UserGroupService {
         return getUserGroup.orElse(null);
     }
 
-    public void addUserGroup(UserGroup newUserGroup) {
-        userGroupRepository.save(newUserGroup);
+    public void saveUserGroup(UserGroup userGroup) {
+        userGroupRepository.save(userGroup);
     }
 
-    public void updateUserGroup(UserGroup getUserGroup) {
-        userGroupRepository.save(getUserGroup);
+    public UserGroup updateUserGroup(Long currentUserGroupId, UserGroup newUserGroupData) {
+        UserGroup currentUserGroup = findUserGroupById(currentUserGroupId);
+        if (newUserGroupData.getName() != null) {
+            currentUserGroup.setName(newUserGroupData.getName());
+        }
+        if (newUserGroupData.getUsers() != null) {
+            currentUserGroup.setUsers(newUserGroupData.getUsers());
+        }
+        saveUserGroup(currentUserGroup);
+        return currentUserGroup;
+    }
+
+    public UserGroupDTO addUserToUserGroup(User user) {
+        UserGroup findUserGroup = findUserGroupById(user.get_userGroup().getId());
+        findUserGroup.getUsers().add(user);
+        UserGroup currentUserGroup = updateUserGroup(user.get_userGroup().getId(), null);
+
+        return convertToUserGroupDTO(currentUserGroup);
     }
 
     protected UserGroupDTO convertToUserGroupDTO(UserGroup userGroup) {
-        return new UserGroupDTO(userGroup.getName());
+        Set<UserDTO> users = userGroup.getUsers()
+                .stream()
+                .map(userService::convertToUserDTO)
+                .collect(Collectors.toSet());
+        return new UserGroupDTO(userGroup.getName(), users);
     }
 }
