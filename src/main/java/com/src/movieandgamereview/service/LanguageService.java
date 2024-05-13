bc3ac2a54704362e2bf54c72b31b3b4ef2b986fb
ellisman31 <akos99@protonmail.com>
2024-05-13 17:13:37 +0200

    Language related modifications

--- src/main/java/com/src/movieandgamereview/service/LanguageService.java
+++ src/main/java/com/src/movieandgamereview/service/LanguageService.java
@@ -1,9 +1,11 @@
 package com.src.movieandgamereview.service;
 
 import com.src.movieandgamereview.dto.LanguageDTO;
+import com.src.movieandgamereview.model.Information;
 import com.src.movieandgamereview.model.Language;
 import com.src.movieandgamereview.repository.LanguageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jdbc.core.mapping.AggregateReference;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -15,18 +17,20 @@ public class LanguageService {
 
     @Autowired
     private LanguageRepository languageRepository;
+    @Autowired
+    private InformationService informationService;
 
     public List<LanguageDTO> getAllLanguage() {
         return ((List<Language>) languageRepository.findAll()).stream().map(this::convertToLanguageDTO).collect(Collectors.toList());
     }
 
-    public Language findLanguageById(Long languageId) {
-        Optional<Language> getLanguage = languageRepository.findById(languageId);
+    public Language findLanguageById(Long currentLanguageId) {
+        Optional<Language> getLanguage = languageRepository.findById(currentLanguageId);
         return getLanguage.orElse(null);
     }
 
-    public LanguageDTO findAndGetLanguageDTOById(Long languageId) {
-        Optional<Language> getLanguage = languageRepository.findById(languageId);
+    public LanguageDTO findAndGetLanguageDTOById(Long currentLanguageId) {
+        Optional<Language> getLanguage = languageRepository.findById(currentLanguageId);
         return getLanguage.map(this::convertToLanguageDTO).orElse(null);
     }
 
@@ -40,6 +44,16 @@ public class LanguageService {
             currentLanguage.setName(newLanguageData.getName());
         }
         saveLanguage(currentLanguage);
+    }
+
+    public void deleteLanguage(Long currentLanguageId) {
+        Language getLanguage = findLanguageById(currentLanguageId);
+        List<Information> informationList = informationService.findInformationByLanguage(AggregateReference.to(currentLanguageId));
+        informationList.forEach(information -> {
+            information.set_language(null);
+            informationService.updateInformation(information.getId(), information);
+        });
+        languageRepository.delete(getLanguage);
     }
 
     protected LanguageDTO convertToLanguageDTO(Language language) {
